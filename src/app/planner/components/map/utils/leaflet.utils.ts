7ebecf1da1d170d120de55aea3352e3af89fe313
l3miage-sbartaim <sbartaiachraf@gmail.com>
2024-05-02 17:54:43 +0200

    ..

--- src/app/planner/components/map/utils/leaflet.utils.ts
+++
@@ -1,7 +0,0 @@
-// import { Map as LeafletMap, LatLng } from "leaflet";
-
-// export function recenter({ leafletMap, latlng }: { leafletMap: LeafletMap, latlng: LatLng }) {
-//     // leafletMap.dragging.disable()
-//     leafletMap.invalidateSize({ animate: false })
-//     leafletMap.setView(latlng, leafletMap.getZoom() ?? 15);
-// }
